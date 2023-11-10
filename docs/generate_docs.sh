7d5fcea8bc8724153bba38ced978515d483d67f2
hari22yuva <56021398+hari22yuva@users.noreply.github.com>
2023-11-10 12:46:11 +0000

    Updated build_ut.sh,.gitignore and generate_docs.sh

--- docs/generate_docs.sh
+++ docs/generate_docs.sh
@@ -3,7 +3,7 @@
 # PLEASE RUN THIS SCRIPT FROM INSIDE THE docs/ DIR SO THAT RELATIVE PATHS SET IN Doxyfile.cfg WORK AS EXPECTED
 
 # In the future this should moved to a fixed verison
-HAL_GENERATOR_VERSION=develop
+HAL_GENERATOR_VERSION=1.2.0
 
 # This will look up the last tag in the git repo, depending on the project this may require modification
 PROJECT_VERSION=$(git describe --tags | head -n1)
